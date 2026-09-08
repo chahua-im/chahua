a4654958196d92875f8019cf9f974afaea3f104b
nanami <nanamicat77@gmail.com>
2026-09-08 15:56:30 +0800

    polyfill

--- pwa/src/main.ts
+++ pwa/src/main.ts
@@ -12,6 +12,14 @@ import { provideChahuaBaseUrl } from './generated/endpoints/chahua.base-url';
 import { authInterceptor } from './app/api/auth.interceptor';
 import { jsonInterceptor } from './app/api/json.interceptor';
 
+// https://stackoverflow.com/a/2117523/2800218
+if (isDevMode() && typeof crypto.randomUUID !== 'function')
+  crypto.randomUUID = function randomUUID() {
+    return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c: string) =>
+      (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16),
+    ) as ReturnType<Crypto['randomUUID']>;
+  };
+
 bootstrapApplication(App, {
   providers: [
     provideBrowserGlobalErrorListeners(),
